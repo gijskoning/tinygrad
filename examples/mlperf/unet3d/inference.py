e712e2a01b4757c86b7abe79b0622e3c28163b1f
Gijs Koning <gijs-koning@live.nl>
2023-09-26 19:15:00 +0200

    profile

--- examples/mlperf/unet3d/inference.py
+++ examples/mlperf/unet3d/inference.py
@@ -59,20 +59,26 @@ from tinygrad.tensor import Tensor
 #                     "eval_loss": eval_loss}
 #
 #     return eval_metrics
-
+from line_profiler_pycharm import profile
+@profile
 def evaluate(flags, model, loader, score_fn, epoch=0):
     s, i = 0, 0
+    eval_cpu = getenv("EVAL_CPU", 1)
     for i, batch in enumerate(tqdm(loader, disable=not flags.verbose)):
       print("eval batch", i)
       image, label = batch
       dtype_img = dtypes.half if getenv("FP16") else dtypes.float
-      image, label = Tensor(image.numpy()[:1], dtype=dtype_img), Tensor(label.numpy()[:1], dtype=dtype_img, device='CPU') # todo added [:1] for overfitting
+
+      image, label = Tensor(image.numpy()[:1], dtype=dtype_img), Tensor(label.numpy()[:1], dtype=dtype_img) # todo added [:1] for overfitting
+
       print('eval image shape',image.shape)
       output, label = sliding_window_inference(model, image, label, flags.val_input_shape)
       del image
-
+      if eval_cpu:
+        output = output.cpu().realize()
+        label = label.cpu().realize()
       print('output.shape', output.shape) #~ (1, 3, 190, 384, 384)
-      s += score_fn(output.cpu(), label).mean().numpy() # to cpu saves a lot of memory
+      s += score_fn(output, label).mean().numpy() # to cpu saves a lot of memory
       del output, label
 
     val_dice_score = s / (i+1)
